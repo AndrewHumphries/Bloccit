de84a40cfec222117063d568a5324a08b4f069ad
AndrewHumphries <ah@harrisonmetal.com>
2013-08-13 21:28:52 -0500

    Readme Update + Created welcome controller with index and about views.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bloccit::Application.config.secret_token = '0ba43d37c5f01e233ef3d4de63f0de23869e1e285e1752cd3c71aa296827a9974393467eeccb215f52b2d9ab77d67416a6a3b92ea37e97373730ad8ce917df20'
